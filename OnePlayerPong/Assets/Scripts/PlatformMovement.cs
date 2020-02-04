4a9e6879cf6e01222f3e230af78cd7821021297a
Conner Anderson <21.conner.anderson@ksd.org>
2020-02-04 09:09:08 -0800

    Basic Controls

---
+++ OnePlayerPong/Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlatformMovement : MonoBehaviour
+{
+    public Joystick joystick;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (joystick.Horizontal > .2)
+        {
+            if (transform.rotation.eulerAngles.z == 0)
+            {
+                transform.position = transform.position + new Vector3(speed, 0, 0);
+            }
+            else if (transform.rotation.eulerAngles.z == 90)
+            {
+                transform.position = transform.position + new Vector3(0, speed, 0);
+            }
+            else if (transform.rotation.eulerAngles.z == 180)
+            {
+                transform.position = transform.position + new Vector3(-speed, 0, 0);
+            }
+            else
+            {
+                transform.position = transform.position + new Vector3(0, -speed, 0);
+            }
+        }
+        if (joystick.Horizontal < -.2)
+        {
+            if (transform.rotation.eulerAngles.z == 0)
+            {
+                transform.position = transform.position + new Vector3(-speed, 0, 0);
+            }
+            else if (transform.rotation.eulerAngles.z == 90)
+            {
+                transform.position = transform.position + new Vector3(0, -speed, 0);
+            }
+            else if (transform.rotation.eulerAngles.z == 180)
+            {
+                transform.position = transform.position + new Vector3(speed, 0, 0);
+            }
+            else
+            {
+                transform.position = transform.position + new Vector3(0, speed, 0);
+            }
+        }
+        if (transform.position.x > 5)
+        {
+            transform.position = new Vector3(5, transform.position.y, 0);
+        }
+    }
+}
